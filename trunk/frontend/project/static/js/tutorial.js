ab95498d01c9371b4ad93217824aea4cb9390f10
vsirisanthana@gmail.com <vsirisanthana@gmail.com@4046c2de-4a89-1cab-7a47-5c1b1e8d2d3b>
2012-02-06 17:42:04 +0000

    Use CardsView

--- trunk/frontend/project/static/js/tutorial.js
+++ trunk/frontend/project/static/js/tutorial.js
@@ -23,6 +23,8 @@ $(function() {
         url: 'http://localhost:8001/cards'
     });
 
+    window.cards = new Cards();
+
     window.Project = Backbone.Model.extend({
         defaults: {
             name: 'Unnamed'
@@ -56,6 +58,31 @@ $(function() {
         }
     });
 
+    window.CardsView = Backbone.View.extend({
+
+        initialize: function() {
+            this.collection.on('add', this.addOne, this);
+            this.collection.on('reset', this.render, this);
+
+            this.collection.fetch();
+        },
+
+        render: function() {
+            this.$el.empty();
+            this.collection.each(this.addOne);
+        },
+
+        addOne: function(card) {
+            var cardView = new CardView({model: card});
+            $('#cards').append(cardView.render().el);
+        }
+    });
+
+    window.cardsView = new CardsView({
+        el: $('#cards'),
+        collection: cards
+    })
+
 
     window.ProjectView = Backbone.View.extend({
 
@@ -66,10 +93,10 @@ $(function() {
         initialize: function() {
             this.model.on('change', this.render, this);
 
-            this.model.get('cards').on('add', this.addOne, this);
+//            this.model.get('cards').on('add', this.addOne, this);
 //            this.model.get('cards').on('all', this.render, this);
 
-            this.model.get('cards').fetch();
+//            this.model.get('cards').fetch();
 
             this.render();
         },
@@ -77,23 +104,22 @@ $(function() {
         render: function() {
             this.$('#project-name').text(this.model.get('name'));
 
-            this.model.get('cards').each(this.addOne);
+//            this.model.get('cards').each(this.addOne);
         },
 
-        addOne: function(card) {
-            var cardView = new CardView({model: card});
-            $('#cards').append(cardView.render().el);
-        },
+//        addOne: function(card) {
+//            var cardView = new CardView({model: card});
+//            $('#cards').append(cardView.render().el);
+//        },
 
         addCard: function() {
             cardNum += 1;
-            this.model.get('cards').add({name: 'New card' + cardNum});
+            cards.create({name: 'New card' + cardNum});
         }
     });
 
     window.project = new Project({
-        name: 'Adios',
-        cards: new Cards()
+        name: 'Adios'
     });
 
     window.projectView = new ProjectView({
